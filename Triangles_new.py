1ea5c343efd2436e2d623c52784f95c56421c251
Marco M <melvern3@gmail.com>
2018-09-19 00:34:12 +0700

    Add files via upload
    simplified

---
+++ Triangles_new.py
@@ -0,0 +1,73 @@
+
+print()
+print("A")
+print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * "") + i * "*")
+    i = i + 1
+    j = j + 1
+
+print("")
+
+
+print("B")
+print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * "*") + i * "")
+    i = i + 1
+    j = j - 1
+
+print("")
+
+
+print ("C")
+print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((i * " ") + j * "*")
+    j = j - 1
+    i = i + 1
+
+print("")
+
+
+print("D")
+print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * " ") + i * "*")
+    j = j - 1
+    i = i + 1
+
+print("")
+
+
+print("E")
+
+print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * " ") + i * "* ")
+    j = j - 1
+    i = i + 1
+
+print("")
+
+print()
+
+
+
+
+
