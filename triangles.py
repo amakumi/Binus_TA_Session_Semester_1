93f79a4b3b330f2b0fa34d34d5f27c81f607a0f8
Marco M <melvern3@gmail.com>
2018-09-19 00:31:31 +0700

    Add files via upload
    simplified

--- triangles.py
+++ triangles.py
@@ -1,38 +1,54 @@
 
-n = 0
-
 print()
 print("A")
 print()
-for i in range(0, 11):
-    n = n + 1
-    for j in range(0, n - 1):
-        print("*", end = "")
-    print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * "") + i * "*")
+    i = i + 1
+    j = j + 1
+
 print("")
 
 
 print("B")
 print()
-for i in range(0, 11):
-    n = n - 1
-    for j in range(0, n + 1):
-        print("*", end = "")
-    print()
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * "*") + i * "")
+    i = i + 1
+    j = j - 1
+
 print("")
 
 
 print ("C")
 print()
-for c in range (11, 0, -1):
-    print((11 - c) * " " + c * "*")
+
+i = 1
+j = 10
+while i <= 10:
+    print((i * " ") + j * "*")
+    j = j - 1
+    i = i + 1
+
 print("")
 
 
 print("D")
 print()
-for d in range (11, 0, -1):
-    print(d * " " + (11 - d) * "*")
+
+i = 1
+j = 10
+while i <= 10:
+    print((j * " ") + i * "*")
+    j = j - 1
+    i = i + 1
+
 print("")
 
 
