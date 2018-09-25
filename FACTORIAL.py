2d46618f3ce051947e0a4fbcd806830cf2ee6df3
Marco M <melvern3@gmail.com>
2018-09-25 12:40:23 +0700

    TASKS

---
+++ FACTORIAL.py
@@ -0,0 +1,11 @@
+num = int(input("Input Num: "))
+
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial = factorial * i
+print("")
+print("---------------------------------------")
+print("           FACTORIAL: ", factorial)
+print("---------------------------------------")
+print("")
